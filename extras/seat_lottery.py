0058d3b86d46a22ae415142991605f674909a681
James Zuber <jzuber@wayfair.com>
2017-05-10 10:56:40 -0400

    Added more extra python exercises.

---
+++ extras/seat_lottery.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Create function to run a weighted lottery for our new seats.
+
+There are 8 possible seats, and 5 people in the lottery.
+
+Each participant gets N tickets to place in lottery for each seat.  They
+also get to rank the seats in order of desending preference.
+
+Given 5 length 8 lists of (seat, rank, tickets), select a person to sit in 
+5 of the 8 available seats.
+"""
+
